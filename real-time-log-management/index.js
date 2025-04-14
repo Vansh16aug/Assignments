11768413ed60999afa3b638892c5030ad2aa11eb
Vansh16aug <kumarvansh16aug@gmail.com>
2025-04-14 20:53:19 +0530

    fixes

--- real-time-log-management/index.js
+++ real-time-log-management/index.js
@@ -9,24 +9,20 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = 3000;
 
-// Serve static files from public folder
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 
-// SSE endpoint
 app.get("/stream", (req, res) => {
   res.setHeader("Content-Type", "text/event-stream");
   res.setHeader("Cache-Control", "no-cache");
   res.setHeader("Connection", "keep-alive");
 
-  // Send a message every second
   const interval = setInterval(() => {
     res.write(
       `data: ${JSON.stringify({ time: new Date().toISOString() })}\n\n`
     );
   }, 1000);
 
-  // Clean up on client disconnect
   req.on("close", () => {
     clearInterval(interval);
   });
